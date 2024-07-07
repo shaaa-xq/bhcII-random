b5f6a431b7a4c5c60380167dc82a8a2b270844cb
shaaa-xq <ayeshacha177@gmail.com>
2024-07-07 22:03:33 +0700

    dikit

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const bhc = ["Draco", "Ebi", "Han", "Jaya", "Jirman", "Kagutam Morgan", "L", "Wu
 let img = document.getElementsByTagName("img");
 let charaName = document.getElementById("name");
 
-function nameGenerate() {
+function randomCharacter() {
   let randomName = Math.floor(Math.random() * 8);
   img.src = "assets/" + randomName + ".png";
   charaName.innerText = bhc[randomName];
 }
+
+function loadResult() {}
+
+function nameGenerate() {}
